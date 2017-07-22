e2845533d4f98cea1cb961cde5bdb9600cfa1f9d
Jon Love <jonlov4@gmail.com>
2017-07-22 02:18:29 -0400

    TLS/SSL try 1

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const express = require('express');
-var app = express();
+const fs = require('fs'),
+    http = require('http'),
+    https = require('https'),
+    express = require('express');
+
+const app = express();
+
+console.log(app.get('env'));
 
 app
   .set("view engine", "hjs")
@@ -10,8 +16,21 @@ app
   })
   .get('/about', (req, res)=>{
     res.render("about");
-  })
-
-  .listen(3000, ()=>{
-    console.log("Server listening on port 3000...");
   });
+
+  if (process.env.NODE_ENV === "production") {
+
+    var options = {
+      key: fs.readFileSync('~/etc/letsencrypt/live/jayvolr.me/privkey.pem'),
+      cert: fs.readFileSync('~/etc/letsencrypt/live/jayvolr.me/cert.pem')
+    };
+
+    var server = https.createServer(options, app).listen(443, ()=>{
+      console.log("TLS server listening on port 443...");
+    })
+
+  }else {
+    app.listen(3000, ()=>{
+      console.log("Server listening on port 3000...");
+    });
+  }
